98814478b307f22f0e778760dd3aaded84d267a9
Dan <jacobydan01@gmail.com>
2024-08-05 09:16:19 -0400

    Initial Commit

---
+++ DessertRecipes/DessertRecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  DessertRecipesApp.swift
+//  DessertRecipes
+//
+//  Created by a on 8/5/24.
+//
+
+import SwiftUI
+
+@main
+struct DessertRecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
